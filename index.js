2a2efe5760a29611826c9ebed007e8fca61c07d5
Thoma02 <122801995+Thoma02@users.noreply.github.com>
2023-02-10 15:28:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = 24;
+let b = 4;
+const addition = a + b;
+const subtraction = a - b;
+const multiplication = a * b;
+const division = a / b;
+const reassign = a * 7
+const reassign2 = b / 4
+
+console.log(a, b, addition, subtraction, multiplication, division, reassign, reassign2);
+let c = 10;
